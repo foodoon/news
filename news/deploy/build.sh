b41d39978f5bc87b4b6c91b1b959b4de89e0a534
foodoo <foodoon@qq.com>
2014-08-30 07:57:25 +0800

    add

--- news/deploy/build.sh
+++ news/deploy/build.sh
@@ -2,7 +2,8 @@ cd .. && git pull
 tomcat_home=/usr/tomcat-news
 mvn package -Dmaven.test.skip=true
 mv assembly/target/news-1.0-SNAPSHOT.war ROOT.war
+mv ${tomcat_home}/webapps/ROOT.war ROOT.war.bak
 rm -rf ${tomcat_home}/webapps/ROOT
-rm -rf ${tomcat_home}/webapps/ROOT.war
+
 cp ROOT.war ${tomcat_home}/webapps/
 
